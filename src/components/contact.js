6cb2a3eceb6ba52b23637ff5f7b8e0dbd1451e38
Merek Smith <mereksmith@hotmail.com>
2019-01-15 21:52:20 -0700

    php link fix

--- src/components/contact.js
+++ src/components/contact.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import axios from 'axios';
 import "../css/contact.css";
 
-const API_PATH = 'http://localhost/react-contact-form/api/contact/index.php';
+const API_PATH = '/index.php';
 
 class Contact extends Component {
 
