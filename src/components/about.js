50bd1c16128f898822f1f41b7b3a653ea6413558
Merek Smith <mereksmith@hotmail.com>
2019-01-19 23:05:16 -0700

    updated about me section

--- src/components/about.js
+++ src/components/about.js
@@ -2,36 +2,115 @@ import React from "react";
 import "../css/about.css";
 
 const About = props => (
-		<div className="about">
-			<div class="parallax bg1">
-				<div className="parallax-caption">
-					<span className="border">Getting to Know Me</span>
-				</div>
-			</div>
+  <div className="about">
+    <div class="parallax bg1">
+      <div className="parallax-caption">
+        <span className="border">Getting to Know Me</span>
+      </div>
+    </div>
 
-			<div className="about-me">
-				<h2>About Me</h2>
-				<p>Parallax scrolling is a web site trend where the background content is moved at a different speed than the foreground content while scrolling. Nascetur per nec posuere turpis, lectus nec libero turpis nunc at, sed posuere mollis ullamcorper libero ante lectus, blandit pellentesque a, magna turpis est sapien duis blandit dignissim. Viverra interdum mi magna mi, morbi sociis. Condimentum dui ipsum consequat morbi, curabitur aliquam pede, nullam vitae eu placerat eget et vehicula. Varius quisque non molestie dolor, nunc nisl dapibus vestibulum at, sodales tincidunt mauris ullamcorper, dapibus pulvinar, in in neque risus odio. Accumsan fringilla vulputate at quibusdam sociis eleifend, aenean maecenas vulputate, non id vehicula lorem mattis, ratione interdum sociis ornare. Suscipit proin magna cras vel, non sit platea sit, maecenas ante augue etiam maecenas, porta porttitor placerat leo.</p>
-			</div>
+    <div className="about-me">
+      <h2>About Me</h2>
+      <p>
+        Parallax scrolling is a web site trend where the background content is
+        moved at a different speed than the foreground content while scrolling.
+        Nascetur per nec posuere turpis, lectus nec libero turpis nunc at, sed
+        posuere mollis ullamcorper libero ante lectus, blandit pellentesque a,
+        magna turpis est sapien duis blandit dignissim. Viverra interdum mi
+        magna mi, morbi sociis. Condimentum dui ipsum consequat morbi, curabitur
+        aliquam pede, nullam vitae eu placerat eget et vehicula. Varius quisque
+        non molestie dolor, nunc nisl dapibus vestibulum at, sodales tincidunt
+        mauris ullamcorper, dapibus pulvinar, in in neque risus odio. Accumsan
+        fringilla vulputate at quibusdam sociis eleifend, aenean maecenas
+        vulputate, non id vehicula lorem mattis, ratione interdum sociis ornare.
+        Suscipit proin magna cras vel, non sit platea sit, maecenas ante augue
+        etiam maecenas, porta porttitor placerat leo.
+      </p>
+    </div>
 
-			<div class="parallax bg2">
-				<div className="parallax-caption">
-					<span className="border">My Work History</span>
-				</div>
-			</div>
+    <div class="parallax bg2">
+      <div className="parallax-caption">
+        <span className="border">My Work History</span>
+      </div>
+    </div>
 
-			<div className="work">
-				<h2>About Me</h2>
-				<p>Parallax scrolling is a web site trend where the background content is moved at a different speed than the foreground content while scrolling. Nascetur per nec posuere turpis, lectus nec libero turpis nunc at, sed posuere mollis ullamcorper libero ante lectus, blandit pellentesque a, magna turpis est sapien duis blandit dignissim. Viverra interdum mi magna mi, morbi sociis. Condimentum dui ipsum consequat morbi, curabitur aliquam pede, nullam vitae eu placerat eget et vehicula. Varius quisque non molestie dolor, nunc nisl dapibus vestibulum at, sodales tincidunt mauris ullamcorper, dapibus pulvinar, in in neque risus odio. Accumsan fringilla vulputate at quibusdam sociis eleifend, aenean maecenas vulputate, non id vehicula lorem mattis, ratione interdum sociis ornare. Suscipit proin magna cras vel, non sit platea sit, maecenas ante augue etiam maecenas, porta porttitor placerat leo.</p>
-			</div>
+    <div className="work">
+      {/* <h2>About Me</h2> */}
+      <p>
+        Parallax scrolling is a web site trend where the background content is
+        moved at a different speed than the foreground content while scrolling.
+        Nascetur per nec posuere turpis, lectus nec libero turpis nunc at, sed
+        posuere mollis ullamcorper libero ante lectus, blandit pellentesque a,
+        magna turpis est sapien duis blandit dignissim. Viverra interdum mi
+        magna mi, morbi sociis. Condimentum dui ipsum consequat morbi, curabitur
+        aliquam pede, nullam vitae eu placerat eget et vehicula. Varius quisque
+        non molestie dolor, nunc nisl dapibus vestibulum at, sodales tincidunt
+        mauris ullamcorper, dapibus pulvinar, in in neque risus odio. Accumsan
+        fringilla vulputate at quibusdam sociis eleifend, aenean maecenas
+        vulputate, non id vehicula lorem mattis, ratione interdum sociis ornare.
+        Suscipit proin magna cras vel, non sit platea sit, maecenas ante augue
+        etiam maecenas, porta porttitor placerat leo.
+      </p>
+    </div>
 
-			<div class="parallax bg3">
-				<div className="parallax-caption">
-					<span className="border">Something Else Here</span>
-				</div>
-			</div>
+    <div class="parallax bg3">
+      <div className="parallax-caption">
+        <span className="border">Schooling</span>
+      </div>
+    </div>
 
-		</div>
+    <div className="school">
+      {/* <h2>About Me</h2> */}
+      <p>
+        Parallax scrolling is a web site trend where the background content is
+        moved at a different speed than the foreground content while scrolling.
+        Nascetur per nec posuere turpis, lectus nec libero turpis nunc at, sed
+        posuere mollis ullamcorper libero ante lectus, blandit pellentesque a,
+        magna turpis est sapien duis blandit dignissim. Viverra interdum mi
+        magna mi, morbi sociis. Condimentum dui ipsum consequat morbi, curabitur
+        aliquam pede, nullam vitae eu placerat eget et vehicula. Varius quisque
+        non molestie dolor, nunc nisl dapibus vestibulum at, sodales tincidunt
+        mauris ullamcorper, dapibus pulvinar, in in neque risus odio. Accumsan
+        fringilla vulputate at quibusdam sociis eleifend, aenean maecenas
+        vulputate, non id vehicula lorem mattis, ratione interdum sociis ornare.
+        Suscipit proin magna cras vel, non sit platea sit, maecenas ante augue
+        etiam maecenas, porta porttitor placerat leo.
+      </p>
+    </div>
+
+    <div class="parallax bg4">
+      <div className="parallax-caption">
+        <span className="border">Personal Interests</span>
+      </div>
+    </div>
+
+    <div className="personal">
+      {/* <h2>About Me</h2> */}
+      <p>
+        Parallax scrolling is a web site trend where the background content is
+        moved at a different speed than the foreground content while scrolling.
+        Nascetur per nec posuere turpis, lectus nec libero turpis nunc at, sed
+        posuere mollis ullamcorper libero ante lectus, blandit pellentesque a,
+        magna turpis est sapien duis blandit dignissim. Viverra interdum mi
+        magna mi, morbi sociis. Condimentum dui ipsum consequat morbi, curabitur
+        aliquam pede, nullam vitae eu placerat eget et vehicula. Varius quisque
+        non molestie dolor, nunc nisl dapibus vestibulum at, sodales tincidunt
+        mauris ullamcorper, dapibus pulvinar, in in neque risus odio. Accumsan
+        fringilla vulputate at quibusdam sociis eleifend, aenean maecenas
+        vulputate, non id vehicula lorem mattis, ratione interdum sociis ornare.
+        Suscipit proin magna cras vel, non sit platea sit, maecenas ante augue
+        etiam maecenas, porta porttitor placerat leo.
+      </p>
+    </div>
+
+    <div class="parallax bg5">
+      <div className="parallax-caption">
+        <span className="border">Thanks for looking!</span>
+      </div>
+    </div>
+
+    {/* end parent div */}
+  </div>
 );
 
 export default About;
