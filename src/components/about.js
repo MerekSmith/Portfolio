eb38b0b3fd25ad8a29878b4cf752efe14d5df892
Merek Smith <mesmith@overstock.com>
2019-01-23 16:21:40 -0700

    updated school section

--- src/components/about.js
+++ src/components/about.js
@@ -59,22 +59,12 @@ const About = props => (
     </div>
 
     <div className="school">
-      {/* <h2>About Me</h2> */}
-      <p>
-        Parallax scrolling is a web site trend where the background content is
-        moved at a different speed than the foreground content while scrolling.
-        Nascetur per nec posuere turpis, lectus nec libero turpis nunc at, sed
-        posuere mollis ullamcorper libero ante lectus, blandit pellentesque a,
-        magna turpis est sapien duis blandit dignissim. Viverra interdum mi
-        magna mi, morbi sociis. Condimentum dui ipsum consequat morbi, curabitur
-        aliquam pede, nullam vitae eu placerat eget et vehicula. Varius quisque
-        non molestie dolor, nunc nisl dapibus vestibulum at, sodales tincidunt
-        mauris ullamcorper, dapibus pulvinar, in in neque risus odio. Accumsan
-        fringilla vulputate at quibusdam sociis eleifend, aenean maecenas
-        vulputate, non id vehicula lorem mattis, ratione interdum sociis ornare.
-        Suscipit proin magna cras vel, non sit platea sit, maecenas ante augue
-        etiam maecenas, porta porttitor placerat leo.
-      </p>
+      {/* <h2>Schooling</h2> */}
+      <span>Certificate of Completion – The Coding Bootcamp</span> <span> - University of Utah, Sandy, Utah</span>
+      <br />
+      <span>Bachelor of Science – Finance</span> <span> - University of Utah, Salt Lake City, Utah </span>
+      <br />
+      <span>Associates of Science – Business</span> <span> - Salt Lake Community College, West Jordan, Utah</span>
     </div>
 
     <div className="parallax bg4">
@@ -84,21 +74,9 @@ const About = props => (
     </div>
 
     <div className="personal">
-      {/* <h2>About Me</h2> */}
+      {/* <h2>Personal Interests</h2> */}
       <p>
-        Parallax scrolling is a web site trend where the background content is
-        moved at a different speed than the foreground content while scrolling.
-        Nascetur per nec posuere turpis, lectus nec libero turpis nunc at, sed
-        posuere mollis ullamcorper libero ante lectus, blandit pellentesque a,
-        magna turpis est sapien duis blandit dignissim. Viverra interdum mi
-        magna mi, morbi sociis. Condimentum dui ipsum consequat morbi, curabitur
-        aliquam pede, nullam vitae eu placerat eget et vehicula. Varius quisque
-        non molestie dolor, nunc nisl dapibus vestibulum at, sodales tincidunt
-        mauris ullamcorper, dapibus pulvinar, in in neque risus odio. Accumsan
-        fringilla vulputate at quibusdam sociis eleifend, aenean maecenas
-        vulputate, non id vehicula lorem mattis, ratione interdum sociis ornare.
-        Suscipit proin magna cras vel, non sit platea sit, maecenas ante augue
-        etiam maecenas, porta porttitor placerat leo.
+        When I am not working on web development, I love to spend time with my family. My newly found, favorite hobby is playing board games. I have over 100 unique, modern board games. Part of my collection includes games that my 5 year old daughter can play but are still very interesting for adults. They can often be very educational as well. I find games to be very stimulating as I love to think and be strategic. I am also a geek at heart and love superheroes, especially Marvel movies, as well as video games.
       </p>
     </div>
 
