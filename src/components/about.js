00a5e6ed2111e9053b0fc1ac038ca9e2a8d6452d
Merek Smith <mesmith@overstock.com>
2019-01-24 16:37:28 -0700

    updated about me section

--- src/components/about.js
+++ src/components/about.js
@@ -1,6 +1,13 @@
 import React from "react";
-import portrait from '../assets/images/profilepic.jpg';
 import "../css/about.css";
+
+import portrait from '../assets/images/profilepic.jpg';
+import uofuLogo from '../assets/images/uofu_logo.png';
+import slccLogo from '../assets/images/slcc_logo.png';
+import webDevLogo from '../assets/images/uofu webdev certificate-small.png';
+import ostkLogo from '../assets/images/ostk_logo.jpg';
+import blyLogo from '../assets/images/bly_logo.png';
+import henkelLogo from '../assets/images/henkel_logo.png';
 
 const About = props => (
   <div className="about">
@@ -21,8 +28,8 @@ const About = props => (
             I am a full stack web developer with a background in Supply Chain and Finance. I recently graduated the Coding Bootcamp from the University of Utah in July 2018. I primarily learned and use the MERN stack and am currently working to continue my education in web development. I am taking several more online courses to further my knowledge, with a focus on React and Redux. I am currently working as a Supply Chain Analyst with Overstock.com but am pursuing a career change into Web Development. I am a quick learner with a strong work ethic and constantly strive to learn more and progress.
       </p>
           <p>
-            When I am not working on web development, I love to spend time with my family. My newly found, favorite hobby is playing board games. I have over 100 unique, modern board games. Part of my collection includes games that my 5 year old daughter can play but are still very interesting for adults. They can often be very educational as well. I find games to be very stimulating as I love to think and be strategic. I am also a geek at heart and love superheroes, especially Marvel movies, as well as video games.
-      </p>
+
+          </p>
         </div>
       </div>
     </div>
@@ -35,21 +42,49 @@ const About = props => (
 
     <div className="work">
       {/* <h2>About Me</h2> */}
-      <p>
-        Parallax scrolling is a web site trend where the background content is
-        moved at a different speed than the foreground content while scrolling.
-        Nascetur per nec posuere turpis, lectus nec libero turpis nunc at, sed
-        posuere mollis ullamcorper libero ante lectus, blandit pellentesque a,
-        magna turpis est sapien duis blandit dignissim. Viverra interdum mi
-        magna mi, morbi sociis. Condimentum dui ipsum consequat morbi, curabitur
-        aliquam pede, nullam vitae eu placerat eget et vehicula. Varius quisque
-        non molestie dolor, nunc nisl dapibus vestibulum at, sodales tincidunt
-        mauris ullamcorper, dapibus pulvinar, in in neque risus odio. Accumsan
-        fringilla vulputate at quibusdam sociis eleifend, aenean maecenas
-        vulputate, non id vehicula lorem mattis, ratione interdum sociis ornare.
-        Suscipit proin magna cras vel, non sit platea sit, maecenas ante augue
-        etiam maecenas, porta porttitor placerat leo.
-      </p>
+
+      <div className="work-container">
+
+        {/* Web Dev Logo */}
+        <div className="job">
+          <span>
+            <a href="https://www.overstock.com" target="_blank" rel="noopener noreferrer">
+              <img src={ostkLogo} alt="Web Dev Logo" className="job-logo" />
+            </a>
+          </span>
+          <span className="company">Overstock.com</span>
+          <span> - Supply Chain Analyst</span>
+          <span> - Mar 2018 - Present </span>
+        </div>
+
+
+        {/* UofU Logo */}
+        <div className="job">
+          <span>
+            <a href="http://www.utah.edu/" target="_blank" rel="noopener noreferrer">
+              <img src={blyLogo} alt="UofU Logo" className="job-logo" />
+            </a>
+          </span>
+          <span className="company"> Boart Longyear </span>
+          <span> - Supply Chain Analyst </span>
+          <span> - Mar 2015 - Mar 2018 </span>
+        </div>
+
+
+        {/* SLCCLogo */}
+        <div className="job">
+          <span>
+            <a href="http://www.slcc.edu/" target="_blank" rel="noopener noreferrer">
+              <img src={henkelLogo} alt="SLCC Logo" className="job-logo" />
+            </a>
+          </span>
+          <span className="company">Henkel (Sun Products Corp)</span>
+          <span> - Customer Supply Chain Analyst</span>
+          <span> - Aug 2012 – Mar 2015 </span>
+        </div>
+
+      </div>
+
     </div>
 
     <div className="parallax bg3">
@@ -58,13 +93,51 @@ const About = props => (
       </div>
     </div>
 
-    <div className="school">
+    <div className="schooling">
       {/* <h2>Schooling</h2> */}
-      <span>Certificate of Completion – The Coding Bootcamp</span> <span> - University of Utah, Sandy, Utah</span>
-      <br />
-      <span>Bachelor of Science – Finance</span> <span> - University of Utah, Salt Lake City, Utah </span>
-      <br />
-      <span>Associates of Science – Business</span> <span> - Salt Lake Community College, West Jordan, Utah</span>
+
+      <div className="schools-container">
+
+        {/* Web Dev Logo */}
+        <div className="school">
+          <span>
+            <a href="https://www.youracclaim.com/badges/f2b648dd-7ec6-4821-a6f6-073a4b4cb359/public_url" target="_blank" rel="noopener noreferrer">
+              <img src={webDevLogo} alt="Web Dev Logo" className="school-logo" />
+            </a>
+          </span>
+          <span className="degree">Certificate of Completion – The Coding Bootcamp</span>
+          <span> - University of Utah, Sandy, Utah</span>
+          <span> - Graduated 2018 </span>
+        </div>
+
+
+        {/* UofU Logo */}
+        <div className="school">
+          <span>
+            <a href="http://www.utah.edu/" target="_blank" rel="noopener noreferrer">
+              <img src={uofuLogo} alt="UofU Logo" className="school-logo" />
+            </a>
+          </span>
+          <span className="degree">Bachelor of Science – Finance</span>
+          <span> - University of Utah, Salt Lake City, Utah </span>
+          <span> - Graduated 2012 </span>
+        </div>
+
+
+        {/* SLCCLogo */}
+        <div className="school">
+          <span>
+            <a href="http://www.slcc.edu/" target="_blank" rel="noopener noreferrer">
+              <img src={slccLogo} alt="SLCC Logo" className="school-logo" />
+            </a>
+          </span>
+          <span className="degree">Associates of Science – Business</span>
+          <span> - Salt Lake Community College, West Jordan, Utah</span>
+          <span> - Graduated 2011 </span>
+        </div>
+
+      </div>
+
     </div>
 
     <div className="parallax bg4">
