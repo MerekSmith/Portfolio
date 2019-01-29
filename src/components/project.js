f435d30f72196073bfe05a6a77b2d14607988099
Merek Smith <mesmith@overstock.com>
2019-01-29 16:17:37 -0700

    working on project css

--- src/components/project.js
+++ src/components/project.js
@@ -5,20 +5,24 @@ import "../css/shadowbox/demo.css";
 import "../css/shadowbox/normalize.css";
 import projectArray from "../project_list.json";
 
+
+
 const Project = props => (
   <ul className="stage clearfix">
     {projectArray.map((project, index) => {
-      return (	
+      return (
         <li className="scene" data-aos="zoom-in-up" data-aos-duration="1500" key={index}>
           <a href={project.link} target="_blank" rel="noopener noreferrer">
             <div className="movie">
-              <div className="poster" />
+              <div className="poster">
+              {/* <header>
+                <h1>{project.title}</h1>
+              </header>
+              <div className="project-image" /> */}
+              </div>
               <div className="info">
                 <header>
                   <h1>{project.title}</h1>
-                  {/* <span className="year">1946</span>
-                <span className="rating">PG</span>
-                <span className="duration">130 minutes</span> */}
                 </header>
                 <p>{project.description}</p>
                 <p>This project used: {project.technologies}</p>
