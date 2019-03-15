83bc45ca4a4e86ca38bbb5f4f2a8e8d034963ff7
Merek Smith <mereksmith@hotmail.com>
2019-03-15 15:10:01 -0600

    Updates

---
+++ precache-manifest.2c293e390cbf79c81295d2eeeecdb020.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a350423b8ca9640930797207d6d457a",
+    "url": "/Portfolio/static/media/sql.1a350423.png"
+  },
+  {
+    "revision": "6c699d40b4a0ace64ffb",
+    "url": "/Portfolio/static/css/main.7f4dbe8f.chunk.css"
+  },
+  {
+    "revision": "c7796c5763ed8b7c7147",
+    "url": "/Portfolio/static/js/1.c7796c57.chunk.js"
+  },
+  {
+    "revision": "3b96116072eb85f5a3a2",
+    "url": "/Portfolio/static/js/runtime~main.3b961160.js"
+  },
+  {
+    "revision": "23b0712fde19a5f22b64f00b2e4ef6e2",
+    "url": "/Portfolio/static/media/parallax_dc_jl_lineup.23b0712f.jpg"
+  },
+  {
+    "revision": "6aa56c6d9d2733676b7ef307bed2b7e2",
+    "url": "/Portfolio/static/media/background_ironman_vs_cap.6aa56c6d.jpg"
+  },
+  {
+    "revision": "96ad8ea453afbdd9e7ac4591d0e91a2b",
+    "url": "/Portfolio/static/media/parallax_avengers_lineup.96ad8ea4.jpg"
+  },
+  {
+    "revision": "13e32c470d0692f80ee30f6de68eeb77",
+    "url": "/Portfolio/static/media/parallax_game_table_blurred.13e32c47.jpg"
+  },
+  {
+    "revision": "3d54ebeefb1c07470bbd691e961c0a11",
+    "url": "/Portfolio/static/media/parallax_re2_rain.3d54ebee.jpg"
+  },
+  {
+    "revision": "2b8d3e62f4808b4b0dd7731d3602f368",
+    "url": "/Portfolio/static/media/background_milky_way.2b8d3e62.jpg"
+  },
+  {
+    "revision": "6664e4691774319c9f2ee080d1ab68e8",
+    "url": "/Portfolio/static/media/m_favicon.6664e469.ico"
+  },
+  {
+    "revision": "4a7e12d702d8862d9b79edf05d23aac7",
+    "url": "/Portfolio/static/media/MERN_stack.4a7e12d7.jpg"
+  },
+  {
+    "revision": "45099ff93121a609e35f6751aa6f9fbb",
+    "url": "/Portfolio/static/media/javascript.45099ff9.png"
+  },
+  {
+    "revision": "9231b32eb40927379ed10e56af5ca1f1",
+    "url": "/Portfolio/static/media/mongoose.9231b32e.png"
+  },
+  {
+    "revision": "75c41c4b68de17bccd4cd5b2813958a0",
+    "url": "/Portfolio/static/media/sequelize.75c41c4b.png"
+  },
+  {
+    "revision": "6c699d40b4a0ace64ffb",
+    "url": "/Portfolio/static/js/main.6c699d40.chunk.js"
+  },
+  {
+    "revision": "3f981acdd2c7c5959db6de937efb4aea",
+    "url": "/Portfolio/static/media/vsc.3f981acd.png"
+  },
+  {
+    "revision": "7384412324c769e447d3c4f86e9ff068",
+    "url": "/Portfolio/static/media/bootstrap-stack.73844123.png"
+  },
+  {
+    "revision": "a714ecc7261cefe35c6dc82266fbde2d",
+    "url": "/Portfolio/static/media/yarn.a714ecc7.png"
+  },
+  {
+    "revision": "11d141ccd4ba6daf948f5f5b8121771e",
+    "url": "/Portfolio/static/media/github_icon.11d141cc.png"
+  },
+  {
+    "revision": "43c47c3dcf9b47466857de6dd22545c4",
+    "url": "/Portfolio/static/media/slcc_logo.43c47c3d.png"
+  },
+  {
+    "revision": "58eaee1f6217c5449e214578448319eb",
+    "url": "/Portfolio/static/media/uofu webdev certificate-small.58eaee1f.png"
+  },
+  {
+    "revision": "8e0cf470a313bce8fb1853d69197561d",
+    "url": "/Portfolio/static/media/ostk_logo.8e0cf470.png"
+  },
+  {
+    "revision": "82807ae1bb8af8a5c47b9313ee109927",
+    "url": "/Portfolio/static/media/bly_logo.82807ae1.png"
+  },
+  {
+    "revision": "fbc105f10fce0bc420c70b77f2802753",
+    "url": "/Portfolio/static/media/henkel_logo.fbc105f1.png"
+  },
+  {
+    "revision": "a6b65e45c38fb4abe4a45a7d9e4aa207",
+    "url": "/Portfolio/static/media/profilepic.a6b65e45.jpg"
+  },
+  {
+    "revision": "dd8c9ebfaff7b402cc7073c7b74fe843",
+    "url": "/Portfolio/static/media/uofu webdev certificate-large.dd8c9ebf.png"
+  },
+  {
+    "revision": "f88cf2633672b32c0f487b77defcec5e",
+    "url": "/Portfolio/static/media/parallax_spiderverse.f88cf263.jpg"
+  },
+  {
+    "revision": "2e6804eee41714ecacfeb4444cbed337",
+    "url": "/Portfolio/static/media/background_rainbow_space.2e6804ee.jpg"
+  },
+  {
+    "revision": "a0827998e2f4df418010e58d5b1f252c",
+    "url": "/Portfolio/index.html"
+  }
+];
