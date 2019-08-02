0396da258c829f7555f5411fbb50e68a99f3e744
Merek Smith <mereksmith@hotmail.com>
2019-08-02 14:35:40 -0600

    updated resume link and colony project

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -115,7 +115,7 @@ const Navbar = props => (
         </Link>
         {/* <div className='resume-container'> */}
         <a
-          href='https://drive.google.com/drive/folders/1JnFQZ9LDNUtdHTU85wx6kZgRWm6cADWk'
+          href='https://drive.google.com/file/d/1EGpwOQ8MrbKNhqYO8cjlcSs6z5_gT2LD/view?usp=sharing'
           target='_blank'
           rel='noopener noreferrer'
           className='btn btn-danger nav-resume-btn'
